4b4a9425ac1753a1149a718494763719ec2ea0f7
thatpix3l <git@thatpix3l.com>
2021-10-10 01:56:26 -0400

    Relay, merge, and forward live chat to frontend

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,8 @@
-module relayplum
+module picoplum
 
 go 1.17
+
+require (
+	github.com/akamensky/argparse v1.3.1
+	github.com/alexandrevicenzi/go-sse v1.6.0
+)
--- backend/src/main.go
+++ backend/src/main.go
@@ -3,25 +3,65 @@ package main
 import (
 	"embed"
 	"fmt"
+	"io"
 	"io/fs"
 	"net/http"
+	"os"
+
+	"github.com/akamensky/argparse"
+	"github.com/alexandrevicenzi/go-sse"
 )
 
-//go:embed public
-var publicFolderFS embed.FS
 
-func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Websocket Endpoint")
+var (
+    //go:embed public
+    embedRootFS embed.FS
+    publicRootFS, _ = fs.Sub(embedRootFS, "public")
+    chatChannel = make(chan []byte)
+    parser = argparse.NewParser("picoplum", "Fresh and tiny tool for aggregating livestream chats into a browser source")
+)
+
+func relayChatEndpoint(w http.ResponseWriter, req *http.Request) {
+    chatMsg, _ := io.ReadAll(req.Body)
+    fmt.Println(string(chatMsg))
+    chatChannel <- chatMsg
+}
+
+func mergedChatEndpoint() *sse.Server{
+    s := sse.NewServer(nil)
+    
+    go func() {
+        for {
+            s.SendMessage("/mergedchat", sse.SimpleMessage(string(<-chatChannel)))
+        }
+    }()
+
+    return s
+}
+
+func setupRoutes() *http.ServeMux{
+
+    mux := http.NewServeMux()
+
+    // Root of server is mapped to root of embedded public folder
+    mux.Handle("/", http.FileServer(http.FS(publicRootFS)))
+
+    // Subpath for forwarding merged chat messages to browser
+    mux.Handle("/mergedchat", mergedChatEndpoint())
+
+    // Subpath where 3rd party services POST json chat messages
+    mux.HandleFunc("/relay", relayChatEndpoint)
+    
+    return mux
+
 }
 
 func main() {
-    fmt.Println("yeah")
+    // Build and parse cli arguments
+    listenPort := parser.String("p", "port", &argparse.Options{Required: false, Default: "4554", Help: "Port to bind, default is 4554"})
+    parser.Parse(os.Args)
 
-    publicContentFS, _ := fs.Sub(publicFolderFS, "public")
+    addr := fmt.Sprintf(":%s", *listenPort)
 
-    mux := http.NewServeMux()
-    mux.Handle("/", http.FileServer(http.FS(publicContentFS)))
-    mux.HandleFunc("/ws", wsEndpoint)
-
-    http.ListenAndServe(":3000", mux)
+    http.ListenAndServe(addr, setupRoutes())
 }
